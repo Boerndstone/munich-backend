5433f7cd5a69c614c077d45069957f4dde2a118a
Bernd Ullmann <berndullmann@Bernds-MacBook-Air.local>
2022-07-26 16:21:57 +0200

    chart

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,8 +18,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
+#use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+#use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
