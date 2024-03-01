3a42f36830f1f3f2665f13d83b1b8b82d1588c6c
Bernd Ullmann <info@berndullmann.de>
2024-03-01 17:25:18 +0100

    svg icons

---
+++ README.md
@@ -0,0 +1,3 @@
+### Icon Library
+
+- Material Design Icons (https://icones.js.org/collection/mdi)
--- assets/controllers/routeparams_controller.js
+++ assets/controllers/routeparams_controller.js
@@ -7,15 +7,13 @@ export default class extends Controller {
     const ratingValue = this.data.get("rating");
     const protectionValue = this.data.get("protection");
     this.starTarget.innerHTML =
-      '<i class="fas fa-star" style="color: rgb(202 138 4)"></i>'.repeat(
-        ratingValue
-      );
+      '<div class="star d-inline-block"></div>'.repeat(ratingValue);
     if (protectionValue == 2) {
       this.protectionTarget.innerHTML =
-        '<i class="fa fa-exclamation-triangle text-danger"></i>';
+        '<div class="exclamation d-inline-block"></div';
     } else if (protectionValue == 3) {
       this.protectionTarget.innerHTML =
-        '<i class="fa fa-ambulance text-danger"></i>';
+        '<div class="skull d-inline-block"></div';
     }
   }
 }
