739309edf0e25d9981aa26a20ac4dfd48d567885
Bernd Ullmann <info@berndullmann.de>
2025-01-20 08:14:31 +0100

    migration

--- migrations/Version20250116110750.php
+++ migrations/Version20250116110750.php
@@ -21,13 +21,13 @@ final class Version20250116110750 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         // $this->addSql('ALTER TABLE topo ADD path JSON DEFAULT NULL, CHANGE rocks_id rocks_id INT NOT NULL');
-        $this->addSql('ALTER TABLE topo ADD CONSTRAINT FK_74A061F576A15D70 FOREIGN KEY (rocks_id) REFERENCES rock (id)');
+        // $this->addSql('ALTER TABLE topo ADD CONSTRAINT FK_74A061F576A15D70 FOREIGN KEY (rocks_id) REFERENCES rock (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         // $this->addSql('ALTER TABLE topo DROP FOREIGN KEY FK_74A061F576A15D70');
-        $this->addSql('ALTER TABLE topo DROP path, CHANGE rocks_id rocks_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE topo DROP path, CHANGE rocks_id rocks_id INT DEFAULT NULL');
     }
 }
